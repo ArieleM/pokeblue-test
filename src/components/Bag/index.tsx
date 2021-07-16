9c37b958a492951b6edccfca2e521f9c2128e226
Ariele Mendonça <ariele_apm@outlook.com>
2021-07-16 20:34:20 -0300

    Add filter pokemon

--- src/components/Bag/index.tsx
+++ src/components/Bag/index.tsx
@@ -1,15 +1,51 @@
 import styles from "./styles.module.scss";
 import { Card } from "../Card";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { api } from "../../service/api";
 interface IBagProps {
   name: string;
+  allPokemon: IAllPokemon[];
 }
-export function Bag({ name }: IBagProps) {
+interface IAllPokemon {
+  name: string;
+  url: string;
+}
+
+interface Pokemon {
+  name: string;
+  base_experience: number;
+  image: string;
+}
+
+export function Bag({ name, allPokemon }: IBagProps) {
+  const [filteredPokemon, setFilteredPokemon] = useState<IAllPokemon[]>([]);
+  const [search, setSearch] = useState("");
+
+  const handleSearchPokemon = (pokemonSearch: string) => {
+    let filteredPokemon = [];
+    if (pokemonSearch.length > 0) {
+      filteredPokemon = allPokemon.filter((pokemon: IAllPokemon) => {
+        return pokemon.name.toUpperCase().includes(pokemonSearch.toUpperCase());
+      });
+    }
+    setFilteredPokemon(filteredPokemon);
+    setSearch(pokemonSearch);
+  };
   return (
     <section className={styles.container}>
       <h2>{name}</h2>
-      <input type="text" placeholder="Digite o nome do pokemon" />
+      <input
+        type="search"
+        onChange={(e) => handleSearchPokemon(e.target.value)}
+        placeholder="Digite o nome do pokemon"
+      />
+      <div className={styles.filter}>
+        {filteredPokemon?.map((pokemon) => (
+          <div className={styles.filterPokemon} key={pokemon.name}>
+            {pokemon.name}
+          </div>
+        ))}
+      </div>
       <div className={styles.bagCards}>
         <Card />
         <Card />
--- src/pages/trade.tsx
+++ src/pages/trade.tsx
@@ -1,13 +1,19 @@
+import { useEffect, useState } from "react";
 import Head from "next/head";
 import styles from "./trade.module.scss";
 import { Bag } from "../components/Bag";
-import { useEffect } from "react";
 import { api } from "../service/api";
 
+interface IAllPokemon {
+  name: string;
+  url: string;
+}
+
 export default function Trade() {
+  const [allPokemon, setallPokemon] = useState<IAllPokemon[]>([]);
   useEffect(() => {
     api.get("pokemon/?limit=1118").then((response) => {
-      console.log(response.data);
+      setallPokemon(response.data.results);
     });
   }, []);
   return (
@@ -16,9 +22,9 @@ export default function Trade() {
         <title>Trade - PokeBlue</title>
       </Head>
       <main className={styles.container}>
-        <Bag name="Jogador 1" />
+        <Bag name="Jogador 1" allPokemon={allPokemon} />
         <button>Troca</button>
-        <Bag name="Jogador 1" />
+        <Bag name="Jogador 1" allPokemon={allPokemon} />
       </main>
     </>
   );
