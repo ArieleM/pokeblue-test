57f75b17ee167889654bdced094313fc2a57e09b
Ariele Mendonça <ariele_apm@outlook.com>
2021-07-17 20:22:31 -0300

    Alter text

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -16,10 +16,10 @@ export default function Home() {
           <h3>A sua calculadora de trocas.</h3>
           <p>Descubra se a troca que deseja realizar é justa!</p>
           <p>
-            Cada treinador deve ofertar de 1 a 6 pokemon em cada troca. Através
-            da soma das experiências de cada pokemon a PokeBlue informará se é
-            uma troca justa ou não. Considerando uma margem de 7% (1% para cada
-            geração), para que nenhum treinador sai perdendo!
+            Cada treinador deve ofertar de 1 a 6 pokémon em cada troca. Através
+            da soma das experiências de cada pokémon, a PokeBlue informará se
+            uma troca é justa ou não, considerando uma margem de 7% (1% para
+            cada geração) para que nenhum treinador saia perdendo!
           </p>
 
           <button>Começar a trocar</button>
