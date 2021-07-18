1dc842114676d4291a12f7d254a087216efc64c7
Ariele Mendonça <ariele_apm@outlook.com>
2021-07-18 14:45:00 -0300

    alter route

--- src/pages/history.tsx
+++ src/pages/history.tsx
@@ -59,7 +59,7 @@ export default function History({ allTrades }: HistoryProps) {
 }
 
 export const getServerSideProps: GetServerSideProps = async () => {
-  const response = await axios.get("/api/history-trade");
+  const response = await axios.get("http://localhost:3000/api/history-trade");
   const allTrades: ITrades[] = await response.data.data;
   return {
     props: {
